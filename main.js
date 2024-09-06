e239dd2e567e7fbf1fc32bd4f8e7f044f1c589bb
MecMik <miki.mikedb@gmail.com>
2024-09-06 10:49:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Esempio di animazione JavaScript o funzione futura
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("Il sito di Michele del bene è stato caricato!");
+});
