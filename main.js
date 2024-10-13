0ece079d93e5298cf9f547ce1d1ae74cb1e6ebd9
MecMik <miki.mikedb@gmail.com>
2024-10-13 12:32:08 +0200

    main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// Funzione per creare più stelle
-function createStars() {
-    const starsContainer = document.createElement('div');
-    starsContainer.classList.add('stars');
-    document.body.appendChild(starsContainer);
-
-    for (let i = 0; i < 100; i++) {
-        const star = document.createElement('div');
-        star.classList.add('star');
-        star.style.top = `${Math.random() * 100}vh`;
-        star.style.left = `${Math.random() * 100}vw`;
-        star.style.animationDuration = `${Math.random() * 10 + 5}s`;
-        starsContainer.appendChild(star);
-    }
-}
-
-createStars();
+document.querySelector('button').addEventListener('click', function() {
+    alert('Sto cercando servizi o strumenti...');
+});
